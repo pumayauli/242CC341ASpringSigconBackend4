bd0cb3e796b4a8972d597bd5fc6c2a5428d6964b
zmamanir@uni.edu.pe <>
2024-11-07 18:48:34 -0500

    add all request methods

--- src/main/java/uni/isw/sigconbackend/controller/PersonaController.java
+++ src/main/java/uni/isw/sigconbackend/controller/PersonaController.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,7 +43,7 @@ public class PersonaController {
     }
     @GetMapping("/find")
     public ResponseEntity<?> findPersonaById(@RequestBody Optional<Persona> persona){        
-        logger.info(">findPersonaById",persona.toString());
+        logger.info(">findPersonaById",persona.get().toString());
         try{
             persona=personaService.findPersonaById(persona.get().getIdPersona());
         }catch(Exception e){
@@ -49,6 +52,49 @@ public class PersonaController {
         }
         if(persona==null)
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.builder().message("Persona not found").build());
+        return ResponseEntity.ok(persona.get());
+    }
+    @PostMapping()
+    public ResponseEntity<?> insertPersona(@RequestBody Persona persona){        
+        logger.info(">insertPersona",persona.toString());
+        Persona newPersona;
+        try{
+            newPersona=personaService.insertPersona(persona);
+        }catch(Exception e){
+            logger.error("Error inesperado",e);
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if(newPersona==null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.builder().message("Persona not insert").build());
+        return ResponseEntity.ok(newPersona);
+    }
+    
+    @PutMapping()
+    public ResponseEntity<?> updatePersona(@RequestBody Persona persona){        
+        logger.info(">updatePersona",persona.toString());
+        Persona newPersona;
+        try{
+            newPersona=personaService.updatePersona(persona);
+        }catch(Exception e){
+            logger.error("Error inesperado",e);
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if(newPersona==null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.builder().message("Persona not update").build());
+        return ResponseEntity.ok(newPersona);
+    }
+    
+    @DeleteMapping()
+    public ResponseEntity<?> deletePersona(@RequestBody Optional<Persona> persona){        
+        logger.info(">deletePersona",persona.get().toString());        
+        try{
+            persona=personaService.findPersonaById(persona.get().getIdPersona());
+            if(persona.isPresent())            
+                personaService.deletePersona(persona.get().getIdPersona());
+        }catch(Exception e){
+            logger.error("Error inesperado",e);
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }        
         return ResponseEntity.ok(persona.get());
     }
     
